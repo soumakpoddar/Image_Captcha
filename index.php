7329f4f0b025ab6ba9bf7669e078d6cc031491d3
Soumak <soumakpoddar55@gmail.com>
2020-10-24 08:44:34 +0530

    Initial Commit

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+    $no1=rand(1,91);
+    $no2=rand(1,91);
+    if($no1!=$no2)
+    {
+
+    }
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Image Captcha</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="container">
+        <div class="box">A</div>
+        <div class="box">B</div>
+        <div class="box">C</div>
+        <div class="box">D</div>
+    </div>
+</body>
+</html>
